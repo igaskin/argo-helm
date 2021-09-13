36e25a2b290bd92b2ff194fdff2d0008c7e25bec
Isaac Gaskin <igaskin@tesla.com>
2021-09-13 11:17:06 -0700

    delete everything in helm chart

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ set -eux
 SRCROOT="$(cd "$(dirname "$0")/.." && pwd)"
 GIT_PUSH=${GIT_PUSH:-false}
 
-rm -rf $SRCROOT/output && git clone -b gh-pages git@github.com:argoproj/argo-helm.git $SRCROOT/output
+rm -rf $SRCROOT/output && git clone -b gh-pages git@github.com:igaskin/argo-helm.git $SRCROOT/output
 
 for dir in $(find $SRCROOT/charts -mindepth 1 -maxdepth 1 -type d);
 do
@@ -18,5 +18,5 @@ cd $SRCROOT/output && git status
 
 if [ "$GIT_PUSH" == "true" ]
 then
-    cd $SRCROOT/output && git add . && git commit -m "Publish charts" && git push git@github.com:argoproj/argo-helm.git gh-pages
+    cd $SRCROOT/output && git add . && git commit -m "Publish charts" && git push git@github.com:igaskin/argo-helm.git gh-pages
 fi
